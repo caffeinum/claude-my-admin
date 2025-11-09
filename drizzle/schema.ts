4c7e668695367d03b48bc710bf38ba9f1e86cf2a
Aleksey Bykhun <theisease@gmail.com>
2025-11-08 16:50:38 -0800

    generate drizzle schema from database

---
+++ drizzle/schema.ts
@@ -0,0 +1,17 @@
+import { pgTable, uniqueIndex, serial, text, timestamp } from "drizzle-orm/pg-core"
+
+import { sql } from "drizzle-orm"
+
+
+export const profiles = pgTable("profiles", {
+	id: serial("id").primaryKey().notNull(),
+	name: text("name").notNull(),
+	email: text("email").notNull(),
+	image: text("image").notNull(),
+	createdAt: timestamp("createdAt", { mode: 'string' }).defaultNow().notNull(),
+},
+(table) => {
+	return {
+		uniqueIdx: uniqueIndex("unique_idx").on(table.email),
+	}
+});
